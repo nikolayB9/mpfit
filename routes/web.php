5ea4afef9381d04fce591e5d50e3705153bb8b3a
Nick <12nick12nick@gmail.com>
2025-02-21 09:36:03 +0300

    add controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\MainController::class, 'index'])
     ->name('main');
+
+Route::resource('products', \App\Http\Controllers\ProductController::class);
+
+Route::resource('orders', \App\Http\Controllers\ProductController::class);
+
+Route::resource('categories', \App\Http\Controllers\ProductController::class);
